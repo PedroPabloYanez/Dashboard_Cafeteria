18178308fe9f43bb26797aab688713665cf40f44
pedro <pedropablo.yanez@hotmail.com>
2021-07-02 02:01:02 -0400

    Se cambia font a titulos

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,8 @@
 class DashboardController < ApplicationController
   def index
-    @last12_sales = Sale.group_by_month(:sale_date, last: 12, format: "%b %Y").order(sale_date: :asc).sum(:price)
-    @last12_sales_count = Sale.group_by_month(:sale_date, last: 12, format: "%b %Y").order(sale_date: :asc).count
-    @last12_sales_avg = Sale.group_by_month(:sale_date, last: 12, format: "%b %Y").order(sale_date: :asc).average(:price)
+    @last12_sales = Sale.group_by_month(:sale_date, last: 12, format: "%b-%Y").order(sale_date: :asc).sum(:price)
+    @last12_sales_count = Sale.group_by_month(:sale_date, last: 12, format: "%b-%Y").order(sale_date: :asc).count
+    @last12_sales_avg = Sale.group_by_month(:sale_date, last: 12, format: "%b-%Y").order(sale_date: :asc).average(:price)
     @last12_sales_origin = Sale.where('sale_date >= ?', 1.year.ago).group(:origin).order(sale_date: :asc).count
     @last6_sales_origin = Sale.where('sale_date >= ?', 6.month.ago).group(:origin).order(sale_date: :asc).count
     @last3_sales_origin = Sale.where('sale_date >= ?', 3.month.ago).group(:origin).order(sale_date: :asc).count
